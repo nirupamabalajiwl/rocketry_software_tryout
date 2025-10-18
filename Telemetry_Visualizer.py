1ea91b61ac299b8a3de94014abd73d4e1ad37032
nirup <nirupama.balajiwl@gmail.com>
2025-10-17 23:02:54 -0700

    starting to utilize matplotlib

--- Telemetry_Visualizer.py
+++ Telemetry_Visualizer.py
@@ -1,2 +1,22 @@
-import matplotlib
-import numpy
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+
+time = []
+altitude = []
+velocity = []
+acceleration = []
+temperature = []
+
+with open("telemetry_sample.csv", newline="") as csvfile:
+    CSV_reader = csv.DictReader(csvfile)  
+    for row in CSV_reader:
+        time.append(int(row["time"]))
+        altitude.append(int(row["alititude"]))
+        velocity.append(int(row["velocity"]))
+        acceleration.append(row["acceleration"])
+        temperature.append(int(row["temperature"]))
+
+
+
+
